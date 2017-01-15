5bd042622a275fee7b907ffd7ed4ffa039e29390
King-Yan Kwan <kykwan@linux.suse>
2017-01-15 12:51:56 +0800

    Turn deserialize code into serialize code

--- native-2-web-serialize.hpp
+++ native-2-web-serialize.hpp
@@ -6,15 +6,13 @@ using namespace std;
 
 constexpr size_t P = serial_size<double>;
 
-template <typename T, typename I> T serialize_number(T t, I &i) {
+template <typename T, typename I> void serialize_number(T t, I &i) {
   static_assert(is_arithmetic<T>::value, "Not an arithmetic type");
   static_assert(is_same<uint8_t, remove_reference_t<decltype(*i)>>::value,
                 "Not dereferenceable or uint8_t iterator");
 
-  T t = 0;
-  copy_n(i, serial_size<T>, reinterpret_cast<uint8_t *>(&t));
-  i += serial_size<T> + calc_padding<P, T>();
-  return t;
+  i = copy_n(reinterpret_cast<uint8_t *>(&t), serial_size<T>, i);
+  i = fill_n(i, calc_padding<P, T>(), 0);
 }
 
 template <typename T, typename I, typename J>
@@ -25,26 +23,26 @@ void serialize_numbers(uint32_t count, J j, I &i) {
   // static_assert(is_same<T, remove_reference_t<decltype(*j)>>::value,
   // "Output iterator does not dereference T");
 
-  generate_n(j, count, [&i]() { return serialize_number<T>(i); });
-  i += calc_padding<P, T>(count);
+  for_each(j, j + count, [&i](const T &t) { serialize_number(t, i); });
+  i = fill_n(i, calc_padding<P, T>(count), 0);
 }
 
 template <typename T, typename I, typename J>
 void serialize_objects(uint32_t count, J j, I &i);
 
-template <typename T, uint32_t N, typename I, typename J>
-void serialize_sequence(J j, I &i, true_type) {
-  serialize_numbers<T>(N, j, i);
+template <typename T, typename I, typename J>
+void serialize_sequence_bounded(uint32_t count, J j, I &i, true_type) {
+  serialize_numbers<T>(count, j, i);
 }
 
-template <typename T, uint32_t N, typename I, typename J>
-void serialize_sequence(J j, I &i, false_type) {
-  serialize_objects<T>(N, j, i);
+template <typename T, typename I, typename J>
+void serialize_sequence_bounded(uint32_t count, J j, I &i, false_type) {
+  serialize_objects<T>(count, j, i);
 }
 
-template <typename T, uint32_t N, typename I, typename J>
-void serialize_sequence(J j, I &i) {
-  serialize_sequence<T, N>(j, i, is_arithmetic<T>{});
+template <typename T, typename I, typename J>
+void serialize_sequence_bounded(uint32_t count, J j, I &i) {
+  serialize_sequence<T>(count, j, i, is_arithmetic<T>{});
 }
 
 template <typename T, typename I, typename J>
@@ -66,19 +64,17 @@ void serialize_sequence(uint32_t count, J j, I &i) {
 
 template <typename T, typename U, typename I, typename A>
 void serialize_associative(const A &a, I &i) {
-  auto count = serialize_number<uint32_t>(i);
-  vector<T> v_t;
-  v_t.reserve(count);
-  vector<U> v_u;
-  v_u.reserve(count);
-  serialize_sequence<T>(count, i, begin(v_t), is_arithmetic<T>{});
-  serialize_sequence<U>(count, i, begin(v_u), is_arithmetic<U>{});
-  a = inner_product(begin(v_t), end(v_t), begin(v_u), a,
-                    [](A &a, pair<T, U> p) {
-                      a.emplace(p);
-                      return a;
-                    },
-                    [](T &t, U &u) { return make_pair(t, u); });
+  serialize_number(a.size(), i);
+  std::vector<T> v_t;
+  v_t.reserve(a.size());
+  std::vector<U> v_u;
+  v_u.reserve(a.size());
+  transform(cbegin(a), cend(a), back_inserter(v_t),
+            [](const pair<T, U> &p) { return p.first; });
+  transform(cbegin(a), cend(a), back_inserter(v_u),
+            [](const pair<T, U> &p) { return p.second; });
+  serialize_sequence_bounded<T>(a.size(), cbegin(v_t), i);
+  serialize_sequence_bounded<U>(a.size(), cbegin(v_u), i);
 }
 
 template <typename I, typename T, size_t... Is>
@@ -88,14 +84,13 @@ template <typename I, typename T>
 auto serialize(const T &t, I &i) -> enable_if_t<is_arithmetic<T>::value> {
   serialize_number(t, i);
 }
-
 template <typename I, typename T, size_t N>
 void serialize(const T (&t)[N], I &i) {
-  serialize_sequence<T, N>(t, i);
+  serialize_sequence_bounded<T>(N, t, i);
 }
 template <typename I, typename T, size_t N>
 void serialize(const array<T, N> &t, I &i) {
-  serialize_sequence<T, N>(cbegin(t), i);
+  serialize_sequence_bounded<T>(N, cbegin(t), i);
 }
 template <typename I, typename T, typename U>
 void serialize(const pair<T, U> &t, I &i) {
@@ -163,12 +158,8 @@ void serialize(const structure<Ts...> &t, I &i) {
 }
 
 template <typename T, typename I, typename J>
-void serialize_objects(uint32_t count, I &i, J j) {
-  generate_n(j, count, [&i]() {
-    T t;
-    serialize(t, i);
-    return t;
-  });
+void serialize_objects(uint32_t count, J j, I &i) {
+  for_each(j, j + count, [&i](const T &t) { serialize(t, i); });
 }
 
 template <typename T, typename I> int serialize_index(const T &t, I &i) {
