0a09f86ecfdefab31cf708a15be6f1f17c420c82
King-Yan Kwan <kwan.king.yan@gmail.com>
2017-04-15 09:11:58 +0800

    Provide devault values for html gatherer.

--- native-2-web.js
+++ native-2-web.js
@@ -303,25 +303,25 @@ function write_multiarray(object, type, extents) {
 function html_number(parent, value, dispatcher) {
   d3.select(parent).attr('class', null);
   let node = d3.select(parent).append('input').attr('type', 'number').node();
-  dispatcher.on('gather', () => value(node.value));
+  dispatcher.on('gather', () => value(node.value || 0));
 }
 
 function html_char(parent, value, dispatcher) {
   d3.select(parent).attr('class', null);
   let node = d3.select(parent).append('input').attr('type', 'text').node();
-  dispatcher.on('gather', () => value(node.value.codePointAt(0)));
+  dispatcher.on('gather', () => value(node.value.codePointAt(0) || 0));
 }
 
 function html_char32(parent, value, dispatcher) {
   d3.select(parent).attr('class', null);
   let node = d3.select(parent).append('input').attr('type', 'text').node();
-  dispatcher.on('gather', () => value(node.value.codePointAt(0)));
+  dispatcher.on('gather', () => value(node.value.codePointAt(0) || 0));
 }
 
 function html_string(parent, value, dispatcher) {
   d3.select(parent).attr('class', null);
   let node = d3.select(parent).append('input').attr('type', 'text').node();
-  dispatcher.on('gather', () => value(node.value));
+  dispatcher.on('gather', () => value(node.value || ''));
 }
 
 function html_structure(parent, value, dispatcher, html, names) {
