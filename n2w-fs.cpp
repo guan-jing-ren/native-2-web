0f83613fb5b709b7e8c52dd9f7e7be3373e257eb
guan-jing-ren <kwan.king.yan@gmail.com>
2017-07-18 21:54:18 +0800

    File list API to test optional values.

--- n2w-fs.cpp
+++ n2w-fs.cpp
@@ -13,9 +13,32 @@ auto set_current_working_directory(const filesystem::path &path) {
   filesystem::current_path(path);
 }
 
+// Options:
+// - Show . and ..
+// - Show hidden files
+// - Column order, sort order
+// - Directory size if directory
+auto list_files(optional<vector<filesystem::path>> paths) {
+  cerr << "Listing files\n";
+  vector<filesystem::directory_entry> list;
+  if (paths)
+    for (auto &&path : *paths)
+      for (auto &&entry : filesystem::directory_iterator{path})
+        list.push_back(entry);
+  else
+    for (auto &&entry :
+         filesystem::directory_iterator{current_working_directory()}) {
+      cerr << entry.path() << '\n';
+      list.push_back(entry);
+    }
+  cerr << "Number of files: " << list.size() << '\n';
+  return list;
+}
+
 plugin plugin = []() {
   n2w::plugin plugin;
   plugin.register_service(DECLARE_API(current_working_directory), "");
   plugin.register_service(DECLARE_API(set_current_working_directory), "");
+  plugin.register_service(DECLARE_API(list_files), "");
   return plugin;
 }();
